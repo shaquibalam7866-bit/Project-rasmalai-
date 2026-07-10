97cc9bac2a988823e1ceb19035a8c9e518325b2d
shaquibalam7866-bit <shaquibalam7866@gmail.com>
2026-07-10 16:10:34 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,19 @@
 const startBtn = document.getElementById("startBtn");
 const welcomeMessage = document.getElementById("welcomeMessage");
+const giftBox = document.getElementById("giftBox");
+const giftText = document.getElementById("giftText");
 
 startBtn.addEventListener("click", function () {
     startBtn.style.display = "none";
     welcomeMessage.style.display = "block";
+
+    setTimeout(function () {
+        giftBox.style.display = "block";
+        giftText.style.display = "block";
+    }, 2000);
+});
+
+giftBox.addEventListener("click", function () {
+    giftBox.innerHTML = "🎉";
+    giftText.innerHTML = "<h2>Happy Birthday My Rasmalai ❤️</h2>";
 });
